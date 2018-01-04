0abd881e2c059ab814e39b137a6a3811b9a7897f
Fred Horch <fred@sparkae.com>
2018-01-03 20:43:20 -0500

    0.0.2 added versions
    When publishing to Atmosphere, discovered that it required version numbers for packages used.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,14 @@
 # Changelog for Meteor Mogul meteor-accounts-vue-unstyled
 
+## 0.0.2 - (2018-01-03)
+
+### Changed
+
+* Added versions to packages required by this one.
+
 ## 0.0.1 - (2018-01-03)
 
 ### Changed
 
-* Publishing to Atmosphere as meteormogul:accounts-ui-unstyled
+* Published to Atmosphere as meteormogul:accounts-ui-unstyled
 * First attempt at VueJS component for loginButtons
--- package.js
+++ package.js
@@ -6,21 +6,21 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.use(['tracker', 'service-configuration', 'accounts-base',
-           'underscore', 'templating@1.2.13', 'session', 'jquery'], 'client');
+  api.use(['tracker@1.1.3', 'service-configuration@1.0.11', 'accounts-base@1.4.1',
+           'underscore@1.0.10', 'templating@1.2.13', 'session@1.1.7', 'jquery@1.11.10'], 'client');
   // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
+  api.imply('accounts-base@1.4.1', ['client', 'server']);
 
   // Allow us to call Accounts.oauth.serviceNames, if there are any OAuth
   // services.
-  api.use('accounts-oauth', {weak: true});
+  api.use('accounts-oauth@1.1.15', {weak: true});
 
   // Allow us to directly test if accounts-password (which doesn't use
   // Accounts.oauth.registerService) exists.
-  api.use('accounts-password', {weak: true});
+  api.use('accounts-password@1.5.0', {weak: true});
 
   // Allow us to use VueJS components on the client
-  api.use('meteormogul:vue-dist', 'client');
+  api.use('meteormogul:vue-dist@2.5.10', 'client');
 
   api.addFiles([
     'accounts_ui.js',
@@ -41,12 +41,12 @@ Package.onUse(function (api) {
   // this package doesn't actually apply these styles; they need to be
   // `@import`ed from some non-import less file.  The accounts-ui package does
   // that for you, or you can do it in your app.
-  api.use('less');
+  api.use('less@2.7.11');
   api.addFiles('login_buttons.import.less');
 });
 
 Package.onTest(function (api) {
   api.use('accounts-ui-unstyled');
-  api.use('tinytest');
+  api.use('tinytest@1.0.12');
   api.addFiles('accounts_ui_tests.js', 'client');
 });
